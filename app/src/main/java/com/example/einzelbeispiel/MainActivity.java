d15bf25ea24cce8381dc3e987f5751c1c2934dca
yahorpaulson <y.paulson@yahoo.com>
2023-03-08 11:59:34 +0100

    Button and Text elements are connected with code

--- app/src/main/java/com/example/einzelbeispiel/MainActivity.java
+++ app/src/main/java/com/example/einzelbeispiel/MainActivity.java
@@ -3,12 +3,35 @@ package com.example.einzelbeispiel;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
+        EditText txtMatrikelNr;
+        Button antwortBtn;
+
+        txtMatrikelNr = findViewById(R.id.editMatrikelNr);
+        antwortBtn = findViewById(R.id.antwortBtn);
+
+
+        //Text verknüpfen
+        String text = txtMatrikelNr.getText().toString();
+
+
+        //Button verknüpfen
+        antwortBtn.setOnClickListener(
+                new View.OnClickListener(){
+                    @Override
+                    public void onClick(View view) {
+                        //TODO: Fuktionalität fürs Button
+                    }
+                }
+        );
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
